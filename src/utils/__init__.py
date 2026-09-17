dc35a8f23e4bb923e6783265078bb1334d67d2aa
reactiveheat <efraimwattimury22@gmail.com>
2026-09-17 11:35:58 +0700

    fix: make FileIdentifier import lazy to avoid forcing python-magic on import

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,12 +3,12 @@
 This module exposes the public API of utilities.
 """
 
-from utils.file_identifier import FileIdentifier
 from utils.hash_calculator import HashCalculator, HashResult
 from utils.hex_dump_viewer import HexDumpConfig, HexDumpSummary, HexDumpViewer
 
+# FileIdentifier is imported from utils.file_identifier directly so importing
+# this package does not require python-magic/libmagic at package-import time.
 __all__ = [
-    "FileIdentifier",
     "HashCalculator",
     "HashResult",
     "HexDumpConfig",
